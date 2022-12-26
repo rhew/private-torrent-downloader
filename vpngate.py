cc8e9fdd04c04ce2eeb2e6241246d11ba5bd9d79
James Rhew <james@rhew.org>
2022-12-26 15:32:14 -0500

    block US

--- vpngate.py
+++ vpngate.py
@@ -8,6 +8,8 @@ import urllib.request
 VPN_CSV_URL = 'http://www.vpngate.net/api/iphone/'
 VPN_FILENAME = 'vpngate.ovpn'
 
+BlockingCountries = ['JP', 'US']
+
 def best_vpngate_score(row1, row2):
     try:
         score1 = int(row1['Score'])
@@ -20,7 +22,7 @@ def best_vpngate_score(row1, row2):
         print('row 2 sucks: {}'.format(row2))
         return row1
     # Japan servers are blocking torrent traffic
-    return row1 if int(row1['Score']) > int(row2['Score']) and row1['CountryShort'] != 'JP' else row2
+    return row1 if int(row1['Score']) > int(row2['Score']) and row1['CountryShort'] not in BlockingCountries else row2
 
 def get_best(rows):
     return functools.reduce(best_vpngate_score, rows)
@@ -38,6 +40,8 @@ def main():
     with urllib.request.urlopen(VPN_CSV_URL) as response:
         next(response) # skip first line with containing "*vpn_servers"
         csv_data = list(csv.DictReader(response.read().decode('utf-8').splitlines()))
+
+    print('Available countries: ' + str(set([row['CountryShort'] for row in csv_data])))
 
     best = get_best(csv_data)
     print('Selected {} in {} running at {} and {} ms latency from {} choices.'.format(
